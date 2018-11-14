57330a691247c16dd32aaf9466f07282a2f2d717
Tarig Bouazzati <tbouzza@gmail.com>
2018-11-14 18:00:33 +0100

    search bar/new

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,44 @@
 class SearchesController < ApplicationController
   def index
   end
+
+  def new
+    @search = Search.new
+  end
+
+  def create
+    if params[:q].present?
+      raise
+      @search = search.new(search_params)
+      @search.address = params[:q]
+      respond_to do |format|
+        if @search.save
+          format.html { redirect_to @search, notice: 'search was successfully created.' }
+          format.json { render :show, status: :created, location: @search }
+        else
+          format.html { render :new }
+          format.json { render json: @search.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      render :new
+    end
+  end
+
+  def show
+    @search = search.find(params[:id])
+    # @mapelement = Array.new
+    # build_mapelement(@search, @mapelement)
+    @marker = [
+      {
+        lat: @search.latitude,
+        lng: @search.longitude
+      }]
+  end
+
+  private
+
+  def search_params
+    params.require(:search).permit(:radius, :address, :latitude, :longitude, :user_id, :commmune_id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_103527) do
+ActiveRecord::Schema.define(version: 2018_11_13_185736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2018_11_13_103527) do
     t.integer "zip_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "destinations", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "pictures", force: :cascade do |t|
