376f6b38a8b1e8fd5567c1f975b49157a75a97a5
Nhu Truong <trng.nhu@gmail.com>
2018-11-14 12:07:11 +0100

    last changes commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'searches#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :communes do
-    resources :searches
-  end
+  resources :searches
 end
