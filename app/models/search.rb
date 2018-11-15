ce9701e07d5203db4cdf2198e1df8a964cee051a
Andrzej Gbur <andrzej.gbur@yahoo.com>
2018-11-15 21:14:17 +0100

    updated review table for geocoder - added address, renamed long, latit

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,10 +1,8 @@
 class Search < ApplicationRecord
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
-  belongs_to :user
-  has_many :reviews, dependent: :destroy
   # the below required by geocode gem
   # so at the moment of saving new search the long/lati will be added based on address
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  belongs_to :user
+  has_many :reviews, dependent: :destroy
 end
