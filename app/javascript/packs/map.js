d2643fc6660b47f5ceed076ee55bfbe2bef55061
Andrzej Gbur <andrzej.gbur@yahoo.com>
2018-11-18 11:14:58 +0100

    working on modal popup

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -3,6 +3,7 @@ import GMaps from 'gmaps/gmaps.js';
 import { autocomplete } from '../components/autocomplete';
 
 const mapElement = document.getElementById('map');
+
 if (mapElement) {
   const map = new GMaps({ el: '#map', lat: 0, lng: 0});
   const markers = JSON.parse(mapElement.dataset.marker);
@@ -25,10 +26,14 @@ if (mapElement) {
 
   // add the main marker (user typed address)
   map.addMarker({
-   lat: marker_main,
-   lng: marker_main,
-   icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-   animation: google.maps.Animation.DROP
+    lat: marker_main,
+    lng: marker_main,
+    icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+    animation: google.maps.Animation.DROP,
+    click: function(){
+      // alert("Hello from the main marker");
+      $('#myModal').modal('show');
+    }
   });
 
   markers.unshift(marker_main);
