bb9646a5bf4c335504709212a417b9c2346b542a
Tarig Bouazzati <tbouzza@gmail.com>
2018-11-20 21:33:49 +0100

    dropped likes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_144811) do
+ActiveRecord::Schema.define(version: 2018_11_20_203122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,14 +58,6 @@ ActiveRecord::Schema.define(version: 2018_11_19_144811) do
     t.float "longitude"
     t.bigint "review_id"
     t.index ["review_id"], name: "index_destinations_on_review_id"
-  end
-
-  create_table "likes", force: :cascade do |t|
-    t.integer "value"
-    t.bigint "review_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["review_id"], name: "index_likes_on_review_id"
   end
 
   create_table "pictures", force: :cascade do |t|
@@ -151,7 +143,6 @@ ActiveRecord::Schema.define(version: 2018_11_19_144811) do
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "reviews"
   add_foreign_key "destinations", "reviews"
-  add_foreign_key "likes", "reviews"
   add_foreign_key "pictures", "reviews"
   add_foreign_key "reviews", "communes"
   add_foreign_key "reviews", "searches"
